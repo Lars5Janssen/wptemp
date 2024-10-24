b17431981d58a4a60787e6395f4214fa4039f601
Lars Janssen <lars.e.janssen@gmail.com>
2024-10-24 20:55:47 +0200

    cube

--- src/main/java/wpcg/lab/a1/TriangleMeshScene.java
+++ src/main/java/wpcg/lab/a1/TriangleMeshScene.java
@@ -41,10 +41,33 @@ public class TriangleMeshScene extends Scene3D {
 
     protected TriangleMesh makeSimpleMesh() {
         wpcg.base.mesh.TriangleMesh mesh = new wpcg.base.mesh.TriangleMesh();
-        mesh.addVertex(new Vector3f(0, 0, 0));
-        mesh.addVertex(new Vector3f(0, 1, 0));
-        mesh.addVertex(new Vector3f(1, 1, 0));
+        mesh.addVertex(new Vector3f(0, 0, 0)); // 0
+        mesh.addVertex(new Vector3f(0, 1, 0)); // 1
+        mesh.addVertex(new Vector3f(1, 1, 0)); // 2
+        mesh.addVertex(new Vector3f(1, 0, 0)); // 3
+
+        mesh.addVertex(new Vector3f(0, 0, 1)); // 4
+        mesh.addVertex(new Vector3f(0, 1, 1)); // 5
+        mesh.addVertex(new Vector3f(1, 1, 1)); // 6
+        mesh.addVertex(new Vector3f(1, 0, 1)); // 7
+
         mesh.addTriangle(new Triangle(0, 1, 2));
+        mesh.addTriangle(new Triangle(0, 2, 3));
+        mesh.addTriangle(new Triangle(5, 4, 6));
+        mesh.addTriangle(new Triangle(6, 4, 7));
+
+        mesh.addTriangle(new Triangle(1, 5, 6));
+        mesh.addTriangle(new Triangle(2, 1, 6));
+
+        mesh.addTriangle(new Triangle(1, 4, 5));
+        mesh.addTriangle(new Triangle(1, 0, 4));
+
+        mesh.addTriangle(new Triangle(2, 6, 7));
+        mesh.addTriangle(new Triangle(3, 2, 7));
+
+        mesh.addTriangle(new Triangle(0, 3, 4));
+        mesh.addTriangle(new Triangle(3, 7, 4));
+
         return mesh;
     }
 
